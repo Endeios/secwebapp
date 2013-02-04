824c01f27b7bf29ac7ae8c2db6dd5626c8f2eb01
endeios <endeios@gmail.com>
2013-02-04 17:52:04 +0100

    aggiunto supporto vaadin con spring

--- secwebapp/src/main/java/org/endeios/security/vaadin/SecWebApp.java
+++ secwebapp/src/main/java/org/endeios/security/vaadin/SecWebApp.java
@@ -2,40 +2,38 @@ package org.endeios.security.vaadin;
 
 import java.util.Date;
 
+import org.endeios.security.service.LogEntryService;
+import org.endeios.security.vaadin.helper.SpringContextHelper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Configurable;
+
 import com.vaadin.Application;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.Window;
 
+//@Configurable(preConstruction = true,dependencyCheck=true)
 public class SecWebApp extends Application {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = -4439997529796272044L;
+	
+	//@Autowired(required=true)
+	private LogEntryService logEntryService;
+
+	private SpringContextHelper sch;
 
 	@Override
 	public void init() {
-		final Window mainWindow = new Window("Myproject Application");
-
-		Label label = new Label("Hello Vaadin user");
-		mainWindow.addComponent(label);
-
-		mainWindow.addComponent(new Button("What is the time?",
-				new Button.ClickListener() {
-					/**
-					 * 
-					 */
-					private static final long serialVersionUID = 1L;
-
-					public void buttonClick(ClickEvent event) {
-						mainWindow
-								.showNotification("The time is " + new Date());
-					}
-				}));
-
+		final Window mainWindow = new Window("Sec Web  Application");
+		sch = new SpringContextHelper(this);
+		logEntryService = (LogEntryService) sch.getBean("logEntryService");
 		setMainWindow(mainWindow);
+		mainWindow.addComponent(new SecWebAppMainWindow());
+		mainWindow.addComponent(new Label(logEntryService.getAllEntries()+""));
 	}
 
 }
